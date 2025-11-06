6e8c85ad72201a85a0a1c91ae9e2b9e52d1800e6
ElineMirabal <elmi0001@stud.ek.dk>
2025-11-06 13:11:48 +0100

    forms-side

--- js/main.js
+++ js/main.js
@@ -9,18 +9,38 @@ document.querySelector("#hotspotPanel").addEventListener("click", clickHead);
 
 function mouseOverHead() {
   console.log("mouseOverHead");
-  document.querySelector("#hotspotPanel").style.fill = "purple";
+  document.querySelector("#hotspotPanel").style.fill = "#9d6fa0";
 }
 
 function mouseOutHead() {
   console.log("mouseOutHead");
-  document.querySelector("#hotspotPanel").style.fill = "green";
+  document.querySelector("#hotspotPanel").style.fill = "#a31c16";
 }
 
 function clickHead() {
   console.log("clickHead");
+  document.querySelector("#efficiency").classList.remove("hide");
+  document.querySelector("#efficiency").classList.add("fadeIn");
+
+  document.querySelector("#requirement").classList.remove("hide");
+  document.querySelector("#requirement").classList.add("fadeIn");
+  document
+    .querySelector("#requirement")
+    .addEventListener("animationend", cleanup);
+
   document.querySelector(".info-text > h2").textContent = "Fodpanel";
-  document.querySelector(".placeholder").textContent = "bla bla bla";
-  document.querySelector("#efficiency").innerHTML = `<h3>Eline</h3>
-   <p> bla bla bla weekend</p>`;
+  document.querySelector(".placeholder").textContent =
+    "Hav altid gode grydelapper klar!";
+  document.querySelector("#efficiency").innerHTML = `<h3>Huskeregl nr.1</h3>
+   <p> Hvis du har gode grydelapper klar når du tager noget ud af ovnen, undgår du at brænde dig og tabe din mad på gulvet!</p>`;
+}
+
+function cleanup() {
+  console.log("cleanup");
+  document
+    .querySelector("#requirement")
+    .removeEventListener("animationend", cleanup);
+
+  document.querySelector("#efficiency").classList.remove("fadeIn");
+  document.querySelector("#requirement").classList.remove("fadeIn");
 }
